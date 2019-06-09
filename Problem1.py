04c49691b45b6b17b145be14f619d6cc942bbbd8
ankit3466 <48724966+ankit3466@users.noreply.github.com>
2019-06-09 21:37:05 +0530

    Create Problem1.py

---
+++ Problem1.py
@@ -0,0 +1,13 @@
+###############
+import datetime
+
+name=input("Enter your Name :")
+age=int(input("Enter your age :"))
+
+# give a message when they will turn in 95 year
+ y=95-age
+ cu_year = datetime.today().year
+ x=cu_year+y
+ print("You will turn at 95 in "+x)
+ 
+ 
