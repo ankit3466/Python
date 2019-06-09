b623c7d07a5eb02acfb3d3acaa4470cccdeef513
ankit3466 <48724966+ankit3466@users.noreply.github.com>
2019-06-09 21:54:53 +0530

    Update Problem2.py

--- Problem2.py
+++ Problem2.py
@@ -11,3 +11,10 @@ l=[]
 # Storing search url in list l
 for i in search(url,stop=10):
         l.append(i)
+
+#Storing file parmanent
+f=open('urls.txt','w')
+for i in l:
+    f.write(i)
+    f.write('\n')
+f.close()
