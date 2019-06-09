5b9b617b98961a3d9a8da5f7561e6c7783df3df4
ankit3466 <48724966+ankit3466@users.noreply.github.com>
2019-06-09 22:30:12 +0530

    Create Problem3.py

---
+++ Problem3.py
@@ -0,0 +1,18 @@
+ adhoc=[1,2,3,1,4,5,66,22,2,6,0,9]
+ l=[]
+ q=[]
+ 
+ # 1.>
+ print("no greater than 5")
+  for i in adhoc:
+    if i > 5 :
+      print(i)
+      l.append(i)
+  print("no less then and equal to 2")
+  for i in adhoc:
+    if i<=2 :
+      print(i)
+      q.append(i)
+      
+      
+      
