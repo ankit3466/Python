1190323ff67f895e57b94bb3f8261e1a096d02da
ankit3466 <48724966+ankit3466@users.noreply.github.com>
2019-06-10 22:07:15 +0530

    Create Problem4.py

---
+++ Problem4.py
@@ -0,0 +1,10 @@
+#########################################################
+import os
+l=[]
+for i in range(10):
+    l.append(str(i))
+    
+name=input("Enter user name :")
+
+for i in name:
+    if(i in)
