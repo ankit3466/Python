663e53d13e82864a716b47b78c8272edd64ede8c
ankit3466 <48724966+ankit3466@users.noreply.github.com>
2019-06-11 09:09:14 +0530

    Problem5  solved

--- Problem4.py
+++ Problem4.py
@@ -1,10 +1,19 @@
 #########################################################
 import os
-l=[]
-for i in range(10):
-    l.append(str(i))
-    
-name=input("Enter user name :")
+import crypt
 
-for i in name:
-    if(i in)
+n=[]
+for i in range(0,9):
+	n.append(str(i))
+username=input('Enter username you want to add :')
+password='hello'+username
+for i in username:
+	if i in n:
+		exit('Cannot create user')
+
+#os.system('sudo useradd  '+username)
+encpass=crypt.crypt(password,'123')
+os.system('sudo useradd -p '+ encpass +' '+username)
+ 
+print("User created")
+    
