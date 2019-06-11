e7ba34eafd7bcd4eedcf1b598e425afa6240437b
ankit3466 <48724966+ankit3466@users.noreply.github.com>
2019-06-11 16:42:49 +0000

    Create Problem8.py

---
+++ Problem8.py
@@ -0,0 +1,21 @@
+###################################################################
+#Run in linux
+
+import subprocess
+
+com=input("Enter a command")
+f=open('command.txt','a+')
+f.seek(0)
+data=f.read()
+
+if com in data:
+	subprocess.getoutput('echo "command is repeted dont do this" | festival --tts')
+else:
+	f=open('command.txt','a+')
+	f.write(str(com))
+	f.write('\n')
+	f.close()
+	if subprocess.getstatusoutput(com)[0] == 0:
+		print(subprocess.getoutput(com))
+	else:
+		subprocess.getoutput('echo "command is incorrect" | festival --tts')
