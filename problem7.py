8e0877fcd1b774726e2233d9aa2269dbd5870913
ankit3466 <48724966+ankit3466@users.noreply.github.com>
2019-06-13 05:07:06 +0000

    touch command using file handling

---
+++ problem7.py
@@ -0,0 +1,29 @@
+op='''
+press 1 for create single file :
+press 2 for multiple file creation :
+press 3 for touch -c command :
+'''
+print(op)
+c=input("Enter your choice")
+
+if(c=='1'):
+	fname=input("enter file name : ")	
+	f=open(fname,'w')
+	f.close()
+	print("File created")
+elif(c=='2'):
+	n=int(input("how many file you want to create : "))
+	l=[]
+	for i in range(n):
+		l.append(input("enter file name : "))
+	for i in l:
+		f=open(i,'w')
+		f.close()
+	print("File created")
+
+elif(c=='3'):
+	a=input("enter file name : ")
+	f=open(a,'r')
+	f.close()
+else:
+	print("Invalid Input...")
