9ecad20d77a272cc10d0330340ceb673cef8f2f5
ankit3466 <48724966+ankit3466@users.noreply.github.com>
2019-06-14 11:34:32 +0530

    Update problem12.py

--- problem12.py
+++ problem12.py
@@ -10,3 +10,19 @@ for i in search(n,stop=3):
 	qr.svg(str(j)+".svg",scale=8)
 	j=j+1
 
+	
+## create a html code for the images
+<html>
+<body>
+<h1> QRCODE for search hello </h1>
+                                                                                                                                                                        
+<img src="1.svg"/>
+                                                                                                                                                                        
+<img src="2.svg"/>
+<img src="3.svg"/>
+</body>
+                                                                                                                                                                        
+</html>
+                                                                                                                                                                        
+## store this code in /var/www/html (you can store images as well in this directory or provides the full path of QR code in img tag )
+
