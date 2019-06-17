75cfe07f36fc22f00312823cc84346e2c85ed926
ankit3466 <48724966+ankit3466@users.noreply.github.com>
2019-06-17 08:53:32 +0000

    Create Problem13.py

---
+++ Problem13.py
@@ -0,0 +1,34 @@
+import pyttsx3
+import datetime as dt
+import os
+
+s=pyttsx3.init()
+s.say("Hey, Ankit Gupta")
+s.runAndWait()
+
+d=dt.datetime.now().hour
+if(d>=6 and d<12):
+        s.say(" Good Morning")
+elif(d>=12 and d<18):
+        s.say(" Good afternoon")
+elif(d>=18 and d<21):
+	s.say(" Good evening")
+else:
+	s.say(" Good night")
+s.runAndWait()
+s.stop()
+def add(*a):
+	sums=0
+	for i in a:
+		sums=sums+i
+	print(sums)
+
+
+def sort(*v):
+	print(sorted(v))
+
+def inst_mod():
+	x=os.system('pip3 list | wc -l')
+	print(x)
+
+
