c41e51b542e7f3421eb1efcaa0d9c9235f69cc34
ankit3466 <48724966+ankit3466@users.noreply.github.com>
2019-06-25 20:18:42 +0530

    sending email from python script

--- Send_email.py
+++ Send_email.py
@@ -1,14 +1,22 @@
+# importing library
 import smtplib
 
+# taking input from user about sender email and password also receiver address
 add=input("Enter your gmail : \n")
 passw=input("Enter your pass : \n")
 to=input("Enter receiver email : \n")
 msg=input("Enter your msg : ")
 
+# setting smtp protocol
 ms=smtplib.SMTP('smtp.gmail.com',587)
 ms.starttls()
+
+# login into sender account
 ms.login(add,passw)
+# sending mail to receiver
 ms.sendmail(add,to,msg)
 
 print("\n ..sent !!")
+
+# logout from the server and quit the connection
 ms.quit()
